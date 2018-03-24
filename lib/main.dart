664cb7c7ef271e8858144c52d2ceefea707edb0e
Tamás Varga <mail@tvarga.hu>
2018-03-24 19:13:31 +0100

    switch to just firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,133 +1,95 @@
-import 'package:cheaplist/redux/actions.dart';
-import 'package:cheaplist/redux/app_state.dart';
-import 'package:cheaplist/redux/app_state_reducer.dart';
-import 'package:cheaplist/redux/middleware.dart';
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
-import 'package:redux_epics/redux_epics.dart';
+
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.blue,
+  accentColor: Colors.orangeAccent[400],
+);
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  final store = new Store<AppState>(
-      appStateReducer,
-      initialState: new AppState(),
-      middleware: [new EpicMiddleware(allEpics)]
-  );
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'CheapList',
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: new MyHomePage(title: 'CheapList'),
+    );
+  }
+}
+
+class MerchantItemList extends StatelessWidget {
+  var merchantId;
+
+  MerchantItemList(String merchantId) {
+    this.merchantId = merchantId;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new StoreProvider(
-      store: store,
-      child: new MaterialApp(
-        title: 'CheapList',
-        theme: new ThemeData.light(),
-        home: new SampleAppPage(),
+    return new StreamBuilder<QuerySnapshot>(
+      stream: getMerchantItems(merchantId),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) return const Text('Loading...');
+        return new ListView(
+          children: snapshot.data.documents.map((DocumentSnapshot document) {
+            return new Column(mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Text(document['name']),
+                  new Text('${document['price']}')
+                ]);
+          }).toList(),
+        );
+      },
+    );
+  }
+
+}
+
+Stream<QuerySnapshot> getMerchantItems(String merchantId) {
+  return Firestore.instance
+      .collection("merchantItems/" + merchantId + "/items")
+      .snapshots;
+}
+
+class MyHomePage extends StatelessWidget {
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+        elevation: Theme
+            .of(context)
+            .platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body:
+      new Row(
+        children: <Widget>[
+          new Expanded (
+            child: new MerchantItemList("3QFbXk5gw0KXfNCZTiOi"),
+          ),
+          new Expanded (
+            child: new MerchantItemList("oS53CrXawnyVOXVf6VKw"),
+          ),
+        ],
       ),
     );
   }
 }
-
-class SampleAppPage extends StatefulWidget {
-  SampleAppPage({Key key}) : super(key: key);
-
-  @override
-  _SampleAppPageState createState() => new _SampleAppPageState();
-}
-
-class _SampleAppPageState extends State<SampleAppPage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return new StoreBuilder(
-        onInit: (store) =>
-            mainStoreInitDispatch(store),
-        onDispose: (store) =>
-            mainStoreDisposeDispatch(store),
-        builder: (context, Store<AppState> store) {
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text("CheapList"),
-            ),
-            body: new Row(
-              children: <Widget>[
-                new Expanded (
-                    child: new ListView.builder(
-                        key: new Key("firstMerchantItemList"),
-                        itemCount: getFirstMerchantItemsLength(store.state),
-                        itemBuilder: (BuildContext context, int position) {
-                          return getFirstMerchantRow(store, position);
-                        })
-                ),
-                new Expanded (
-                    child: new ListView.builder(
-                        key: new Key("secondMerchantItemList"),
-                        itemCount: getSecondMerchantItemsLength(store.state),
-                        itemBuilder: (BuildContext context, int position) {
-                          return getSecondMerchantRow(store, position);
-                        })
-                ),
-              ],
-            ),
-          );
-        }
-    );
-  }
-
-  int getFirstMerchantItemsLength(AppState state) {
-    if (state == null) {
-      return 0;
-    }
-    if (state.firstMerchantItems == null) {
-      return 0;
-    }
-    return state.firstMerchantItems.length;
-  }
-
-  Widget getFirstMerchantRow(Store store, int i) {
-    return new GestureDetector(
-      child: new Padding(
-          padding: new EdgeInsets.all(10.0),
-          child: new Column(mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Text(store.state.firstMerchantItems[i].name),
-                new Text('${store.state.firstMerchantItems[i].price}')
-              ])),
-    );
-  }
-
-  int getSecondMerchantItemsLength(AppState state) {
-    if (state == null) {
-      return 0;
-    }
-    if (state.secondMerchantItems == null) {
-      return 0;
-    }
-    return state.secondMerchantItems.length;
-  }
-
-  Widget getSecondMerchantRow(Store store, int i) {
-    return new GestureDetector(
-      child: new Padding(
-          padding: new EdgeInsets.all(10.0),
-          child: new Column(mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Text(store.state.secondMerchantItems[i].name),
-                new Text('${store.state.secondMerchantItems[i].price}')
-              ])),
-    );
-  }
-
-  void mainStoreInitDispatch(Store store) {
-    store.dispatch(new RequestFirstMerchantDataEventsAction());
-    store.dispatch(new RequestSecondMerchantDataEventsAction());
-  }
-
-  mainStoreDisposeDispatch(Store store) {
-    store.dispatch(new CancelFirstMerchantDataEventsAction());
-    store.dispatch(new CancelSecondMerchantDataEventsAction());
-  }
-
-}
-
